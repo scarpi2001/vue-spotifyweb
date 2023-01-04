4c511325a326e4698e6086c73af5c5a62378f14a
Marco <posta.email3001@gmail.com>
2023-01-04 12:49:41 +0100

    creato componente ContentTop e importato in MainContent

--- src/components/main_components/MainContent.vue
+++ src/components/main_components/MainContent.vue
@@ -1,16 +1,34 @@
 <script>
+import ContentTop from './content_components/ContentTop.vue'
 export default {
     name: "MainContent",
     components: {
-
+        ContentTop,
     }
 }
 </script>
 
 <template>
+    <div id="top_bar">
+        <ContentTop />
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../../styles/partials/variables" as*;
 @use "../../styles/general.scss" as*;
+
+#top_bar {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 90px;
+    width: calc(100% - 200px);
+    background-color: rgba(0, 0, 5, 0.5);
+    padding: 0px 30px;
+    z-index: 999;
+}
 </style>
---
+++ src/components/main_components/content_components/ContentTop.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: "ContentTop",
+}
+</script>
+
+<template>
+
+    <a class="btn link" href="#">
+        EFFETTUA L'UPGRADE
+    </a>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../../../styles/partials/variables" as*;
+@use "../../../styles/general.scss" as*;
+
+.btn {
+    border: 1px solid white;
+    border-radius: 20px;
+    padding: 10px 30px;
+    color: white;
+    transition: all 0.5s;
+
+    &:hover {
+        scale: 1.2;
+    }
+
+    .link {
+        color: #fff;
+    }
+}
+</style>
